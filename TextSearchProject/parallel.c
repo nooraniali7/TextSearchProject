099f57357d7196d81c64b0a8aed651849b2362cf
Gurkirt Heerey <GurkirtHeerey@dhcp-10-15-146-187.wireless.pacific.edu>
2018-04-22 22:01:07 -0700

    Testing

--- TextSearchProject/parallel.c
+++ TextSearchProject/parallel.c
@@ -10,29 +10,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
-/*Brute Force Method
- *COMP 137 Project
- *
- *Compile: gcc -o main main.c
- *Run: ./main <text to find> <file to search>
- *
- *
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+// GLOBAL VARIABLES
 #define MAXCHAR 100
+int thread_count = 4;
 
-void searchingString(char textSearch[], char fileContents[]);
+void* threadWork(void* p);
 
 int main(int argc, const char * argv[]) {
+    
     if(argc != 3){
         printf("Invalid number of arguments: <executable> <text to find> <file to search>\n");
         return 0;
     }
+    
+    // thread initialization
+    long thread;
+    pthread_t thread_handles;
+    
+    // creating array of thread handles
+    thread_handles = malloc(thread_count*sizeof(pthread_t));
+    
+    for (thread = 0; thread < thread_count; thread++)
+    {
+        pthread_create(&thread_handles[thread], NULL, threadWork, (void*) thread);
+    }
+    
+    printf("HELLO FROM MAIN THREAD\n\n\n");
     char textSearch[MAXCHAR];
     char fileName[MAXCHAR];
     char path[MAXCHAR] = "./testFiles/";
